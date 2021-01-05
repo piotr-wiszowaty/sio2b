978b21c121cdbdd4877d75552ad84201ad156de9
Piotr Wiszowaty <pwiszowaty458@gmail.com>
2021-01-05 23:29:09 +0100

    siolib: Use python2 in bootloader generator

--- siolib/src/bootloader/generate-bootloader.sh
+++ siolib/src/bootloader/generate-bootloader.sh
@@ -3,4 +3,4 @@
 cd `dirname $0`
 xasm /o:bootloader.obx /l:/tmp/bootloader.lst bootloader.asx || exit 1
 xasm /o:bootloader_hs.obx /l:/tmp/bootloader_hs.lst bootloader_hs.asx || exit 1
-python dump.py ../main/java/pw/atari/sio/XEX.java
+python2 dump.py ../main/java/pw/atari/sio/XEX.java
